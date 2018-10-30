8f317d535f5066278bb71e522137116ca1e57dc0
1337 <kong1337@qq.com>
2018-10-30 16:55:39 +0800

    add: remote serve

--- app/src/main/java/com/example/administrator/ezReader/ui/fragment/MainHaYuBookFragment.java
+++ app/src/main/java/com/example/administrator/ezReader/ui/fragment/MainHaYuBookFragment.java
@@ -1,6 +1,6 @@
 package com.example.administrator.ezReader.ui.fragment;
 
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,9 +11,9 @@ import android.view.ViewGroup;
 
 import com.example.administrator.ezReader.R;
 import com.example.administrator.ezReader.bean.HayuBook;
+import com.example.administrator.ezReader.util.ConnUtil;
 import com.example.library.BaseAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -41,21 +41,29 @@ public class MainHaYuBookFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_ha_yu_book, container, false);
         ButterKnife.bind(this,view);
-        List<HayuBook> books = new ArrayList<>();
-        for (int i = 0; i < 20; i ++){
-            HayuBook book = new HayuBook(String.valueOf(i),"第" + i +"本书");
-            books.add(book);
-        }
-        BaseAdapter adapter = new BaseAdapter.Builder()
-                .setDataList(books)
-                .build();
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
-
+        new GetHaYuBook().execute();
         return  view;
     }
 
+    class GetHaYuBook extends AsyncTask<Void,Void,List<HayuBook>>{
+
+        @Override
+        protected List<HayuBook> doInBackground(Void... voids) {
+           return ConnUtil.sendMsg();
+        }
+
+        @Override
+        protected void onPostExecute(List<HayuBook> hayuBooks) {
+            List<HayuBook> books = hayuBooks;
+            BaseAdapter adapter = new BaseAdapter.Builder()
+                    .setDataList(books)
+                    .build();
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(adapter);
+        }
+
+    }
 }
--- app/src/main/java/com/example/administrator/ezReader/util/ConnUtil.java
+++ app/src/main/java/com/example/administrator/ezReader/util/ConnUtil.java
@@ -2,6 +2,8 @@ package com.example.administrator.ezReader.util;
 
 import android.util.Log;
 
+import com.example.administrator.ezReader.bean.HayuBook;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +11,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnUtil {
     private static final String TAG = "ConnUtil";
@@ -43,6 +47,42 @@ public class ConnUtil {
             Log.d(TAG, "Conn2Server: 连接失败！");
             e.printStackTrace();
         }
+    }
+
+    public static List<HayuBook> sendMsg(){
+        try {
+            System.out.println("ready to conn...");
+            Socket socket = new Socket("132.232.118.114",9999);
+            System.out.println("conn success...");
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            System.out.println("send msg..");
+            pw.write("00001,\n");
+            pw.flush();
+            System.out.println("send success..");
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String temp;
+            StringBuffer sb = new StringBuffer();
+            System.out.println("ready receive...");
+            List<HayuBook> books = new ArrayList<>();
+            while ((temp = br.readLine())!=null){
+                Log.d(TAG, "sendMsg: 333" + temp);
+                String[] msg = temp.split(",");
+                books.add(new HayuBook(msg[0],msg[1]));
+            }
+            Log.d(TAG, "sendMsg: the size of book is :" + books.size());
+            os.close();
+            socket.close();
+            isr.close();
+            is.close();
+            return books;
+        } catch (IOException e) {
+            System.out.println("conn error");
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
